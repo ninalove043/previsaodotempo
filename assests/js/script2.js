0813809ef718b9df8748d12436e0aaf7cc8c525f
ninalove043 <decamipa2@gmail.com>
2024-07-01 20:55:45 -0300

    Add files via upload

---
+++ assests/js/script2.js
@@ -0,0 +1,29 @@
+// Chave da api
+const key = 'df76991302551e2743f23d05555bf312';
+
+const button = ...;
+const nomeCidade = ...;
+const temperatura ...= 
+const previsao = ...;
+const umidade = ...;
+const imagem = ...;
+
+
+// URL DA API OPEN WEATHER
+// `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`
+
+
+// BUSCAR CIDADE
+async function buscarCidade() {
+
+
+}
+
+// FUNÇÂO procurar
+function cliqueBotao() {
+    
+}
+
+
+
+button.addEventListener('', ...);
